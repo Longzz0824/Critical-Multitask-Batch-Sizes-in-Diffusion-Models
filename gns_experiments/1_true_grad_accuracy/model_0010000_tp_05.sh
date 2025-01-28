e2cffb8ee800041fe2b260d77be1d1cf755b787b
her_username <egecimsir99@gmail.com>
2025-01-28 16:52:59 +0100

    first experiment results

---
+++ gns_experiments/1_true_grad_accuracy/model_0010000_tp_05.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python compute_gns.py --model 0010000.pt --csv_path /home/c/cimsir/GenAI_Practical/gns_experiments/1_true_grad_accuracy/results/model_0010000_tp_05.csv --vis_dir /home/c/cimsir/GenAI_Practical/gns_experiments/1_true_grad_accuracy/visuals -acc -nw -p 0.5 -r 5 
---
+++ gns_experiments/1_true_grad_accuracy/model_0750000_tp_01.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python compute_gns.py --model 0750000.pt --csv_path /home/c/cimsir/GenAI_Practical/gns_experiments/1_true_grad_accuracy/results/model_0750000_tp_01.csv --vis_dir /home/c/cimsir/GenAI_Practical/gns_experiments/1_true_grad_accuracy/visuals -acc -nw -p 0.1 -r 5 
